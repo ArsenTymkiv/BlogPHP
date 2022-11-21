e56057c9ebfef5b6002e0046344dc182e503d184
Arseny Tymkiv <arseny.tymkiv.ua@gmail.com>
2022-11-21 14:56:24 +0200

    add sql query

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
+
+use PhpMyAdmin\Sql;
+
 include("./include/head.php");
+$sql = "SELECT * FROM `articles`";
+$result = mysqli_query($connection, "SELECT * FROM `articles`");
+var_dump($result);
 ?>
 
 <body>
@@ -17,103 +23,26 @@ include("./include/head.php");
                             <h3>Нові пости в блозі</h3>
                             <div class="block__content">
                                 <div class="articles articles__horizontal  new_post">
+                                    <?php
+                                    $articles_select = mysqli_query($connection, "SELECT * FROM `articles` ORDER BY `id` DESC LIMIT 6");
+                                    while ($articles = mysqli_fetch_assoc($articles_select)) {
 
-
-                                    <article class="article">
-                                        <div class="article__image" style="background-image: url('./img/black-photo.jpg');">
-                                        </div>
-                                        <div class="article__info">
-                                            <a href="./article.php">Розробка на Node.JS</a>
-                                            <div class="article__info__meta">
-                                                <small>Категорія: <a href="#">Програмування</a></small>
-
+                                    ?>
+                                        <article class="article">
+                                            <div class="article__image" style="background-image: url('./img/<?php echo $articles["img"] ?>');">
                                             </div>
-                                            <div class="article__info__preview">Lorem ipsum dolor sit amet, consectetur
-                                                adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                                                magna ...
-                                            </div>
-                                        </div>
-                                    </article>
+                                            <div class="article__info">
+                                                <a href="./article.php"><?php echo substr($articles["title"], 0, 15);?></a>
+                                                <div class="article__info__meta">
+                                                    <small>Категорія: <a href="#">Програмування</a></small>
 
-                                    <article class="article">
-                                        <div class="article__image" style="background-image: url('./img/black-photo.jpg');">
-                                        </div>
-                                        <div class="article__info">
-                                            <a href="./article.php">Розробка на Node.JS</a>
-                                            <div class="article__info__meta">
-                                                <small>Категорія: <a href="#">Програмування</a></small>
+                                                </div>
+                                                <div class="article__info__preview"><?php echo substr($articles["text"], 0, 100);?> ...
+                                                </div>
+                                            </div>
+                                        </article>
+                                    <?php   }?>
 
-                                            </div>
-                                            <div class="article__info__preview">Lorem ipsum dolor sit amet, consectetur
-                                                adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                                                magna ...
-                                            </div>
-                                        </div>
-                                    </article>
-
-                                    <article class="article">
-                                        <div class="article__image" style="background-image: url('./img/black-photo.jpg');">
-                                        </div>
-                                        <div class="article__info">
-                                            <a href="./article.php">Розробка на Node.JS</a>
-                                            <div class="article__info__meta">
-                                                <small>Категорія: <a href="#">Програмування</a></small>
-
-                                            </div>
-                                            <div class="article__info__preview">Lorem ipsum dolor sit amet, consectetur
-                                                adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                                                magna ...
-                                            </div>
-                                        </div>
-                                    </article>
-
-                                    <article class="article">
-                                        <div class="article__image" style="background-image: url('./img/black-photo.jpg');">
-                                        </div>
-                                        <div class="article__info">
-                                            <a href="./article.php">Розробка на Node.JS</a>
-                                            <div class="article__info__meta">
-                                                <small>Категорія: <a href="#">Програмування</a></small>
-
-                                            </div>
-                                            <div class="article__info__preview">Lorem ipsum dolor sit amet, consectetur
-                                                adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                                                magna ...
-                                            </div>
-                                        </div>
-                                    </article>
-
-                                    <article class="article">
-                                        <div class="article__image" style="background-image: url('./img/black-photo.jpg');">
-                                        </div>
-                                        <div class="article__info">
-                                            <a href="./article.php">Розробка на Node.JS</a>
-                                            <div class="article__info__meta">
-                                                <small>Категорія: <a href="#">Програмування</a></small>
-
-                                            </div>
-                                            <div class="article__info__preview">Lorem ipsum dolor sit amet, consectetur
-                                                adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                                                magna ...
-                                            </div>
-                                        </div>
-                                    </article>
-
-                                    <article class="article">
-                                        <div class="article__image" style="background-image: url('./img/black-photo.jpg');">
-                                        </div>
-                                        <div class="article__info">
-                                            <a href="./article.php">Розробка на Node.JS</a>
-                                            <div class="article__info__meta">
-                                                <small>Категорія: <a href="#">Програмування</a></small>
-
-                                            </div>
-                                            <div class="article__info__preview">Lorem ipsum dolor sit amet, consectetur
-                                                adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                                                magna ...
-                                            </div>
-                                        </div>
-                                    </article>
                                 </div>
                             </div>
                         </div>
